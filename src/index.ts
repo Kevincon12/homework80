591ecc6170b581e571362e0b1e78d2bd6b2e27a2
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-03-03 20:52:16 +0600

    Инициализировали приложение

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'HOMEWORK80 is running' });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
